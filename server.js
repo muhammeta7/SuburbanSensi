8936e0a22ca32f88a6aa92e86b5c66b3892c0efe
Muhammet <muhammeta7@gmail.com>
2017-04-24 11:41:32 -0400

    initial site files with npm packages installed

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var port = process.env.PORT|| 3000;
+app.listen(port, function() {
+  console.log('App listening on PORT: ' + port);
+});
