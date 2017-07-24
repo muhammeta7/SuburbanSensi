441fb5038cbad3cec33b6e02dbeb4f2f2b067cea
Muhammet <muhammeta7@gmail.com>
2017-07-24 18:18:48 -0400

    updated event timer

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ jQuery(function($) {'use strict',
 	
 	//Countdown js
 	 $("#countdown").countdown({
-			date: "12 july 2017 19:00:00",
+			date: "19 august 2017 17:00:00",
 			format: "on"
 		},
 		
